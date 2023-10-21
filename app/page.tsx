d4b35fad0f0f993967699b5325b2390f0ca6edf5
Shourya Naik <sjnaik02@gmail.com>
2023-10-22 00:11:40 +0530

    icon added

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import { data } from "@/lib/data";
 import { columns, Paper } from "./columns";
 import { DataTable } from "./data-table";
+import { Microscope } from "lucide-react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center py-24 bg-background dark">
-      <h1 className="text-3xl font-semibold text-foreground pb-6">
-        Research Paper Indexer
+      <h1 className="text-3xl font-semibold text-foreground pb-6 ">
+        <Microscope className="inline" /> Research Paper Indexer
       </h1>
       <div className="max-w-6xl text-foreground w-full">
         <DataTable columns={columns} data={data} />
